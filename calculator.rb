098440d7a924ae3c6a893d2a33d69a93655212a0
jadeKing <dev@jadeking.co.uk>
2015-09-25 02:38:57 +0100

    refactored calculate_rate

--- calculator.rb
+++ calculator.rb
@@ -2,12 +2,6 @@ def calculate_rate(rates, request)
     amounts = rates.sort_by(&:first)
     temp = []
     loan_count = 0
-    # i = 0
-    # begin
-    #   loan_count = loan_count + amounts[i][1]
-    #   temp.push(amounts[i])
-    #   i = i + 1
-    # end until loan_count > request
     sum = 0
     amounts.each do |x, y|
       loan_count = loan_count + y
@@ -15,7 +9,5 @@ def calculate_rate(rates, request)
       temp.push([x,y])
       break if loan_count > request
     end
-    # temp.each { |x, y| sum = sum + x }
     result = (sum / temp.length * 100).round(1)
-    # result.round(1)
 end
