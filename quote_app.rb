57bff041817cefc1210a00383db0acc2008035a7
jadeKing <dev@jadeking.co.uk>
2015-09-27 19:12:43 +0100

    add run program file

---
+++ quote_app.rb
@@ -0,0 +1,21 @@
+require './quote'
+require './calculator'
+require 'csv'
+require './format_csv'
+require './validate_quote_input'
+
+class QuoteApp
+  include Calculator
+  include FormatCsv
+  include ValidateQuoteInput
+end
+
+def run_quote(market, amount, qa)
+  csv = market
+  result =  qa.calculate_quote(qa.format_file(csv), ARGV[1])
+  final = result == nil ? "Not enough money for your loan" : Quote.new(result).display_quote
+  puts final
+end
+qa = QuoteApp.new
+errors = qa.validate_input(ARGV[1], ARGV[0])
+errors == nil ? (run_quote(ARGV[0], ARGV[1], qa)) : (puts errors)
